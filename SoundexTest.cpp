f07ac6104c7a52d661831293993a957a08ed9320
Mattan-Qwer <post@mnagl.eu>
2022-02-20 23:07:30 +0100

    one letter words are coded correctly

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -8,17 +8,6 @@ int main(int argc, char **argv) {
   return RUN_ALL_TESTS();
 }
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-  Soundex soundex;
-  auto encoded = soundex.encode("A");
-  EXPECT_EQ(encoded, "A");
-  EXPECT_EQ(soundex.encode("B"), "B");
-  EXPECT_EQ(soundex.encode("E"), "E");
-  EXPECT_EQ(soundex.encode("G"), "G");
-  EXPECT_EQ(soundex.encode("ö"), "ö");
-  EXPECT_EQ(soundex.encode("z"), "z");
-}
-
 TEST(SoundexEncoding, PattedSoleLetterOfOneLetterWord) {
   Soundex soundex;
   EXPECT_EQ(soundex.encode("G"), "G000");
